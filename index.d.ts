a8c5bc90a28f44711c1570de1ababca1558b6501
Marek Augustynowicz <marek.augustynowicz@verkstedt.com>
2022-02-02 15:15:56 +0100

    TypeScript type declarations

---
+++ index.d.ts
@@ -0,0 +1,4 @@
+export const createSwimlanesHash: (markup: string) => string
+export const createSwimlanesUrl: (markup: string) => URL
+export const decodeSwimlanesHash: (hash: string) => string
+export const decodeSwimlanesUrl: (url: URL) => string | null
