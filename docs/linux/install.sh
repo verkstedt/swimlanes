0876af311e81d3aff489b7590a3b0f4e699c165e
Sven Nielsen <sven.nielsen@verkstedt.com>
2022-02-16 13:52:43 +0100

    prepare system, and make install.sh work as a download ;)

--- docs/linux/install.sh
+++ docs/linux/install.sh
@@ -16,14 +16,26 @@ then
     exit 70
 fi
 
-sed "s~^Exec=swimlanes~Exec=${bin}~" swimlanes.desktop > swimlanes-local.desktop
+echo "[Desktop Entry]
+Type=Application
+Name=Swimlanes.io
+Exec=${bin} -f %f
+Icon=go-last
+StartupNotify=false
+MimeType=text/x-swimlanes.io
+Categories=Utility;
+Terminal=false
+Comment=Open a swimlanes.io compatible text file in browser on swimlanes.io
+" > /tmp/swimlanes-local.desktop
 
 set -x
 
 # Install Desktop Entry file
 # (see <https://specifications.freedesktop.org/desktop-entry-spec/latest/>)
-desktop-file-install --dir="$HOME/.local/share/applications" ./swimlanes-local.desktop
+desktop-file-install --dir="$HOME/.local/share/applications" /tmp/swimlanes-local.desktop
 update-desktop-database "$HOME/.local/share/applications"
+
+rm /tmp/swimlanes-local.desktop
 
 # Install text/x-swimlanes.io mime type
 # (see <https://specifications.freedesktop.org/shared-mime-info-spec/shared-mime-info-spec-0.21.html#idm46211030056080>)
